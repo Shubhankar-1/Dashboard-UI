454c3d02a3d2d00dbd09e974a374bc0a9a449e81
Shubhankar <shubhankar.nautiyal21@gmail.com>
2023-07-08 23:00:53 +0530

    Added avatar component

---
+++ src/components/Avatars.jsx
@@ -0,0 +1,27 @@
+import { Avatar } from "@mui/material";
+import Image from "next/image";
+import React from "react";
+
+function Avatars({ image, width = 45, height = 45, dotColor }) {
+    return (
+        <div className="relative">
+            {dotColor ? (
+                <div className={` absolute -top-2 right-1 z-10`}>
+                    <svg
+                        fill={dotColor}
+                        width="20px"
+                        height="20px"
+                        viewBox="0 0 14 14"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path d="M7.8 10a2.2 2.2 0 0 0 4.4 0 2.2 2.2 0 0 0-4.4 0z" />
+                    </svg>
+                </div>
+            ) : null}
+            <Avatar sx={{ width: width, height: height }}>
+                <Image src={image} alt="avatar" />
+            </Avatar>
+        </div>
+    );
+}
+
+export default Avatars;
